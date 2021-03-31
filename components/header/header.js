490954033bb48c3c004d9a8b12374be5199bc374
alastairelms <69348143+alastairelms@users.noreply.github.com>
2021-03-31 10:49:54 +0100

    CI/CD test

---
+++ components/header/header.js
@@ -0,0 +1,7 @@
+export default function Header( children, ...restProps ) {
+    return (
+        <div className={styles.header-container}>
+            <h1 className={styles.header-title}>Perudo</h1>
+        </div>
+    )
+}
